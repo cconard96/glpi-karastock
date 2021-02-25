842eecc6ab0fe11fbdca41bbdfe3ab7b2d61a1e2
Karhel <karheltmarr@hotmail.fr>
2021-02-25 23:42:38 +0100

    xml update - version 0.0.7

--- setup.php
+++ setup.php
@@ -26,7 +26,7 @@
  --------------------------------------------------------------------------
  */
 
-define('PLUGIN_KARASTOCK_VERSION', '1.0.7');
+define('PLUGIN_KARASTOCK_VERSION', '0.0.7');
 
 // Minimal GLPI version, inclusive
 define("PLUGIN_KARASTOCK_MIN_GLPI", "9.5");
